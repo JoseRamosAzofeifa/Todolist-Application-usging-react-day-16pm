6d8bb6aaa8eb163017bcef64b19b579905530688
JoseRamosAzofeifa <jara.zff@gmail.com>
2021-03-24 20:22:23 +0000

    Todolist Application usging react

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,54 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState, Fragment } from "react";
 export function Home() {
+	const [todos, setTodos] = useState([]);
+
+	function addTodo(e) {
+		if (e.key === "Enter") {
+			let newTask = e.target.value;
+			let myTodos = todos.slice();
+			myTodos.push(newTask);
+			e.target.value = "";
+			setTodos(myTodos);
+		}
+	}
+	function deleteTodo(i) {
+		let myTodos = todos.slice();
+		let todoDeleted = myTodos.splice(i, 1);
+		setTodos(myTodos);
+	}
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid">
+			<h1 className="text-center">todos</h1>
+			<div className="d-flex justify-content-center col-lg-6 col-md-6 col-12 mx-auto flex-column bg-white">
+				<input
+					className="w-100 border-bottom py-3 pl-4"
+					placeholder="What need to be done?"
+					onKeyPress={addTodo}></input>
+				{todos.length == 0 ? (
+					<div className="border-bottom pt-1 d-flex align-items-center justify-content-between task">
+						<p className="pt-1 pl-4">No task, add a task</p>
+					</div>
+				) : (
+					todos.map(function(todo, i) {
+						return (
+							<div
+								key={i}
+								className="border-bottom pt-1 d-flex align-items-center justify-content-between task">
+								<p className="pt-1 pl-4">{todo}</p>
+								<button
+									className="pb-2"
+									onClick={() => deleteTodo(i)}>
+									<i className="fas fa-times"></i>
+								</button>
+							</div>
+						);
+					})
+				)}
+
+				<div className="border-bottom d-flex align-items-center justify-content-between pb-1">
+					<span className="pt-1">{todos.length + " "}item left</span>
+				</div>
+			</div>
 		</div>
 	);
 }
